8d6016bb6e0da5e46a5052519e0aed9ce97dd00d
YKuts <yuliiakuts@gmail.com>
2024-07-06 22:01:53 +0200

    fix blog, add courses

---
+++ data/moduls.js
@@ -0,0 +1,102 @@
+[
+    {
+        "id": 1,
+        "title": "How the Internet Works",
+        "objectives": [
+            "Understanding the basics of the internet",
+            "Navigating web browsers",
+            "Understanding HTTP, HTTPS"
+        ],
+        "duration": "3 days",
+        "src": "/assets/icons/public.svg"
+    },
+    {
+        "id": 2,
+        "title": "HTML & CSS",
+        "objectives": [
+            "Introduction to HTML",
+            "Creating and structuring web pages",
+            "Adding text, links, and images",
+            "Introduction to CSS",
+            "Applying styles to HTML elements"
+        ],
+        "duration": "3 weeks",
+        "src": "/assets/icons/public.svg"
+    },
+    {
+        "id": 3,
+        "title": "Version Control & Hosting",
+        "objectives": [
+            "Understanding version control systems",
+            "Introduction to Git and GitHub",
+            "Basic Git commands",
+            "Collaborating on projects with Git",
+            "Hosting websites on platforms like GitHub Pages"
+        ],
+        "duration": "2 weeks",
+        "src": "/assets/icons/public.svg"
+    },
+    {
+        "id": 4,
+        "title": "JavaScript Basics",
+        "objectives": [
+            "Introduction to JavaScript",
+            "Variables, data types, and operators",
+            "Control structures (if-else, loops)",
+            "Functions and scope",
+            "Basic DOM manipulation"
+        ],
+        "duration": "3 weeks",
+        "src": "/assets/icons/public.svg"
+    },
+    {
+        "id": 5,
+        "title": "Advanced JavaScript",
+        "objectives": [
+            "Advanced functions (callbacks, promises, async/await)",
+            "Object-oriented programming in JavaScript",
+            "Working with APIs",
+            "Understanding ES6+ features"
+        ],
+        "duration": "4 weeks",
+        "src": "/assets/icons/public.svg"
+    },
+    {
+        "id": 6,
+        "title": "Front-End Frameworks",
+        "objectives": [
+            "Introduction to front-end frameworks (React, Angular, Vue)",
+            "Creating components",
+            "State management",
+            "Routing in single-page applications"
+        ],
+        "duration": "4 weeks",
+        "src": "/assets/icons/public.svg"
+    },
+    {
+        "id": 7,
+        "title": "Back-End Development",
+        "objectives": [
+            "Introduction to back-end development",
+            "Node.js and Express.js",
+            "Working with databases (SQL, NoSQL)",
+            "Creating RESTful APIs",
+            "Authentication and authorization"
+        ],
+        "duration": "5 weeks",
+        "src": "/assets/icons/public.svg"
+    },
+    {
+        "id": 8,
+        "title": "Full-Stack Project",
+        "objectives": [
+            "Planning a full-stack application",
+            "Developing the front-end",
+            "Developing the back-end",
+            "Integrating front-end and back-end",
+            "Deploying the application"
+        ],
+        "duration": "4 weeks",
+        "src": "/assets/icons/public.svg"
+    }
+]
