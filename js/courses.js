6df66edb8d17a093110c4f52e0fed81e4718f6eb
YKuts <yuliiakuts@gmail.com>
2024-07-07 21:46:04 +0200

    add style to courses page

--- js/courses.js
+++ js/courses.js
@@ -16,10 +16,13 @@ function displayCourses(moduls) {
     moduls.forEach(course => {
         const courseElement = document.createElement('div');
         courseElement.classList.add('col-md-6', 'col-lg-4', 'aos-init', 'aos-animate');
+        courseElement.setAttribute('data-aos', 'flip-left');
+        courseElement.setAttribute('data-aos-delay', '100');
+        courseElement.setAttribute('data-aos-duration', '1000');
+
+        const objectivesList = course.objectives.map(objective => `<li>${objective}</li>`).join('');
 
         courseElement.innerHTML = `
-        
-            
                         <div class="courses">
                             <div class="d-flex">
                                 <div class="icon"><span>   
@@ -29,18 +32,17 @@ function displayCourses(moduls) {
                                     <h2>${course.title}</h2>
                                     <p>Objectives:</p>
                                     <ul>
-                                        <li>Understanding the basics of the internet</li>
-                                        <li>Navigating web browsers</li>
-                                        <li>Understanding HTTP, HTTPS</li>
+                                        ${objectivesList}
                                     </ul>
                                     
-                                    <p class="lec"><a href="#">${course.duration}</a></p>
+                                    <p class="lec"><a href="#">Duration: ${course.duration}</a></p>
                                 </div>
                             </div>
                             
                             <div class="text-center">
-                                <a href="#" class="btn d-block btn-primary btn-outline-primary py-3" style="color:aliceblue">Start Now!</a>
+                                <a href="#" class=" d-block btn-custom  py-3">Start Now!</a>
                             </div>
+                            
                         </div>
                     
         `;
